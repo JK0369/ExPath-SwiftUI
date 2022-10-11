92a75b68d01404cdc00f7cf05e5c187774e20e6e
jake <palatable7@naver.com>
2022-10-12 00:01:15 +0900

    Initial Commit

---
+++ ExPath/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExPath
+//
+//  Created by 김종권 on 2022/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExPath/ExPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExPathApp.swift
+//  ExPath
+//
+//  Created by 김종권 on 2022/10/12.
+//
+
+import SwiftUI
+
+@main
+struct ExPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
